dd7dd3dc5dfa67f78de2f5f2d021939c48495f65
clembells <clem.belliot@gmail.com>
2016-02-19 16:32:29 +0100

    user json

--- src/BackyBack/CookieMeetBundle/Controller/MessengerController.php
+++ src/BackyBack/CookieMeetBundle/Controller/MessengerController.php
@@ -8,7 +8,19 @@
 
 namespace BackyBack\CookieMeetBundle\Controller;
 
+use Guzzle\Http\Message\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Doctrine\Tests\Common\DataFixtures\TestEntity\User;
+use FOS\RestBundle\Controller\Annotations\View;
+use Guzzle\Http\Message\Response;
+use Hoa\Websocket\Server as Serve;
+use Hoa\Socket\Server as Socket;
+use Hoa\Event\Bucket as Bucket;
+use Hoa\Event\Source as Source;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 /**
  * Class MessengerController
@@ -26,12 +38,78 @@ class MessengerController extends Controller
     }
 
     /**
-     * @return mixed
+     * @View()
+     * @ParamConverter("user", class="BackyBackCookieMeetBundle:User")
      */
-    public function getCurrentUserAction()
+    private function getCurrentUserAction()
     {
-        $currentUser = $this->getUser();
-        return $currentUser;
+        $user = $this->getUser();
+    }
+
+    /**
+     * @Route("/api/messenger")
+     * @Method("POST")
+     */
+    private function getUsersToJSONAction(Request $request)
+    {
+        $user = $this->getDoctrine()->getRepository('BackyBackCookieMeetBundle:User');
+        $request = $this->post('/api/messenger', [
+            'users' => json_encode($user)
+        ]);
+        return $request;
+    }
+
+    /**
+     * @Route("/api/messenger")
+     * @Method("GET")
+     */
+    private function getUsersToMessageAction(Request $request)
+    {
+        $user = $this->getUsersToJSONAction($request);
+        $user = $this->get('/api/messenger', [
+            'user_decode' => json_decode($user)
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return Request
+     * Create a server and allows users to communicate
+     */
+    private function createServerAction()
+    {
+        $server = new Serve(
+            new Socket('tcp://127.0.0.1:8889')
+        );
+        $server->on('message', function (Bucket $bucket) {
+            $data = $bucket->getData();
+
+            echo 'message: ', $data['message'], "\n";
+            $bucket->getSource()->send($data['message']);
+        });
+        $server->run();
+    }
+
+    /**
+     * @Route("/api/messenger")
+     * @Method("POST")
+     * Get previous message sent into JSON
+     */
+    private function postMessagetoJSONAction(Request $request)
+    {
+        $response = $this->createServerAction();
+
+        return $request;
+    }
+
+    /**
+     * @Route("/api/messenger")
+     * @Method("GET")
+     * Get previous message sent and decode it
+     */
+    private function getMessagefromJSONAction(Request $request)
+    {
+        $response = $this->postMessagetoJSONAction($request);
     }
 
 }
